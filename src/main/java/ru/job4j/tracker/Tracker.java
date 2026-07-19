9891af24e724650544457e85e984d96a530284ec
Pashtet <dexyo1337@yandex.ru>
2026-07-19 10:36:49 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,46 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] result = new Item[items.length];
+        int size = 0;
+        for (Item item : items) {
+            if (item != null) {
+                if (item.getName().equals(key)) {
+                    result[size] = item;
+                    size++;
+                }
+            }
+        }
+        return Arrays.copyOf(result, size);
+    }
+
+    public Item[] findAll() {
+        Item[] result = new Item[items.length];
+        int size = 0;
+        for (Item item : items) {
+            if (item != null) {
+                result[size] = item;
+                size++;
+            }
+        }
+        return Arrays.copyOf(result, size);
+    }
+
+    public static void main(String[] args) {
+        Tracker tracker = new Tracker();
+        Item first = new Item("First");
+        Item second = new Item("Second");
+        tracker.add(first);
+        tracker.add(second);
+        tracker.add(new Item("First"));
+        tracker.add(new Item("Second"));
+        tracker.add(new Item("First"));
+        Item[] result = tracker.findByName(first.getName());
+        for (Item item : result) {
+            System.out.println(item.getName());
+        }
     }
 }
