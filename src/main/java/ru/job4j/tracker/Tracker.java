d07124f6a5aed8785d830c0c3d7a433776ccbcdc
Pashtet <dexyo1337@yandex.ru>
2026-07-19 19:43:25 +0300

    7. Метод удаления заявки Tracker.delete [#211749]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -64,4 +64,13 @@ public class Tracker {
         items[index] = item;
         return true;
     }
+
+    public void delete(int id) {
+        int index = indexOf(id);
+        if (index != -1) {
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
+            size--;
+            items[size] = null;
+        }
+    }
 }
