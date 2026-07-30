793ad44a9da085cee7f7f2d223fcedfd76062e0c
Pashtet <dexyo1337@yandex.ru>
2026-07-31 01:18:52 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,26 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int result = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                result = i;
+                break;
+            }
+        }
+        if (result == -1) {
+            throw new ElementNotFoundException("Element: " + key + " not found");
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        try {
+            int index = indexOf(new String[] {"1", "2", "3"}, "4");
+            System.out.println(index);
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
