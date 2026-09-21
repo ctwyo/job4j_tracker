57be8f729823ab2193137aa614f2fe44b0b3c8d9
dexyo1337 <dexyo1337@yandex.ru>
2026-09-21 14:58:39 +0300

    5. Очередь с приоритетом на LinkedList [#41670 #567106]

--- src/test/java/ru/job4j/search/PriorityQueueTest.java
+++ src/test/java/ru/job4j/search/PriorityQueueTest.java
@@ -7,20 +7,22 @@ public class PriorityQueueTest {
     @Test
     public void whenHigherPrioritySecond() {
         PriorityQueue queue = new PriorityQueue();
+        Task expected = new Task("urgent", 1);
         queue.put(new Task("low", 5));
-        queue.put(new Task("urgent", 1));
+        queue.put(expected);
         queue.put(new Task("middle", 3));
         Task result = queue.take();
-        assertThat(result.getDescription()).isEqualTo("urgent");
+        assertThat(result).isEqualTo(expected);
     }
 
     @Test
     public void whenHigherPriorityEquals() {
         PriorityQueue queue = new PriorityQueue();
-        queue.put(new Task("low", 5));
+        Task expected = new Task("low", 1);
+        queue.put(expected);
         queue.put(new Task("urgent", 5));
         queue.put(new Task("middle", 5));
         Task result = queue.take();
-        assertThat(result.getDescription()).isEqualTo("low");
+        assertThat(result).isEqualTo(expected);
     }
 }
